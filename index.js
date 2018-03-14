bc11bc82896cf905c80887c23c26ebe4ce26abe6
Diego Paez <diego.paez@despegar.com>
2018-03-14 14:22:35 -0300

    Update: require conflict files + building deps

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var css = require('sheetify')
 var choo = require('choo')
+var conflict = require('./lib/conflict')
 
 css('tachyons')
 
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
 }
 
-app.use(require('./stores/clicks'))
-
 app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
+app.route('*', require('./views/404'))
 
 if (!module.parent) app.mount('body')
 else module.exports = app
